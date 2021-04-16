d0b8cccb065f2286f455c8465b43e4f01a369ce7
toonarmycaptain <toonarmycaptain@hotmail.com>
2021-04-16 02:40:05 -0500

    Add form buttons/logic to xkcd route/template.
    NB dropdown select comic choices must be added to form in order to pass
    validation. These must be cast to int as they are strings in the request.

--- API_exploration/apis.py
+++ API_exploration/apis.py
@@ -47,6 +47,10 @@ def xkcd_comic():  # Function named to avoid name clash with API import.
 
     By default return latest xkcd comic.
 
+    select_comic_number dropdown options have to be prepared and attached to
+    form before validation, or validation for that field will fail, even if it
+    was not submitted.
+
     TODO Return desired comic number/date via form.
     ? allow search by date? have to build up db of dates
     ? this would not be too hard as json for comics has the dates as mm dd yyyy
@@ -56,12 +60,32 @@ def xkcd_comic():  # Function named to avoid name clash with API import.
     requested_comic_number = None  # Default, will return latest.
     requested_comic_date = date.today()
 
-    if request.method == 'POST':
-        requested_comic_number = request.form['requested_comic_number']
-        requested_comic_date = request.form['requested_comic_date']
+    form = xkcd.xkcdForm()
 
+    if request.method == 'POST':
+        form.select_comic_number.choices = [  # Prep valid choices to validate dropdown options.
+            num for num in range(1, int(form.latest_comic_number.data) + 1)]
+        if form.validate_on_submit():
+            if form.first.data:
+                requested_comic_number = 1
+            elif form.previous.data:
+                requested_comic_number = int(form.current_comic.data) - 1
+            elif form.comic_number_selected.data:
+                requested_comic_number = form.select_comic_number.data
+            elif form.next.data:
+                requested_comic_number = int(form.current_comic.data) + 1
+            elif form.latest.data:
+                requested_comic_number = None
+    print(form.errors.items())
     comic_data = xkcd.get_comic_data(comic_number=requested_comic_number,
                                      day=requested_comic_date)
+    # Prepare form:
+    comic_choices = [num for num in range(1, comic_data['latest_comic_number'] + 1)]
+    comic_choices.remove(404)  # Remove nonexistent comic
+    form.select_comic_number.choices = comic_choices
+    form.select_comic_number.choices.remove(comic_data['comic_number'])  # Remove current comic
+    form.current_comic.data = comic_data['comic_number']  # Set current comic
+    form.latest_comic_number.data = comic_data['latest_comic_number']  # Set latest comic
 
     return render_template('APIs/xkcd.html',
                            comic_number=comic_data['comic_number'],
@@ -70,7 +94,7 @@ def xkcd_comic():  # Function named to avoid name clash with API import.
                            comic_title=comic_data['comic_title'],
                            comic_alt_text=comic_data['comic_alt_text'],
                            latest_comic_number=comic_data['latest_comic_number'],
-                           )
+                           form=form)
 
 
 @bp.errorhandler(CSRFError)
